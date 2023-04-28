29cc0bb60140a6c403e73507ca9bb5399c2db613
Herb Sneed <hmsneed@gmail.com>
2023-04-27 22:39:29 -0700

    Airplane Work smh

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,79 @@
 var citySearchInput = document.getElementById('citySearchJS')
 var searchContainer = document.getElementById('searchContainerJS');
 var citySearchBtn = document.getElementById('searchSubbtnJS');
-var cityBtn = document.createElement('div');
+var featureCity = document.getElementById('city');
 
+
+var cityName = [];
+
+function renderedCities() {
+var savedCities = JSON.parse(localStorage.getItem('CityName'));
+  if(savedCities !== null) {
+    for (var i = 0; i < savedCities.length; i++) {
+      var savedCityBtn = document.createElement('div');
+      savedCityBtn.innerHTML = savedCities[i];
+      savedCityBtn.setAttribute('class', 'city');
+      searchContainer.appendChild(savedCityBtn);
+    }
+  }
+}
+window.onload = renderedCities();
 
 var search = function(event) {
   event.preventDefault();
 
-  var city = citySearchInput.value.trim();
-
   var getcity = function(city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=c82d8df7a3206dc1d57ce04e07f52165';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=c82d8df7a3206dc1d57ce04e07f52165&units=imperial';
+
     fetch(apiUrl)
       .then(function (response) {
         if(response.ok) {
         response.json()
           .then(function(data) {
-           console.log(data, city);
           });
         }else{
           alert("Error: " + response.statusText);
         };
       })
       .catch(function(error) {
-        alert('Unable to connect ot api');
-      })
+        alert('Unable to connect to api');
+      });
+
   };
-  var cityArray = [];
 
-if (city) {
-  cityArray.push(searchContainer.appendChild(cityBtn));
-  cityBtn.innerHTML = city;
-  cityBtn.setAttribute('class', 'city')
-  searchContainer.appendChild(cityBtn);
-  getcity(city);
-} else {
-  alert("please enter city");
-}
+ var get5Day = function(fiveDay) {
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=c82d8df7a3206dc1d57ce04e07f52165&units=imperial';
 
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json()
+      .then(function (data) {
+        console.log(data);
+      })
+    }else{
+      alert("Unable to connect to api")
+    }
+  })
+ }
+ get5Day();
 
+  if (city !== null) {
+    var city = citySearchInput.value.trim();
+    var cityBtn = document.createElement('div');
+    cityName.push(city);
+    cityBtn.innerHTML = city;
+    featureCity.innerHTML = city;
 
+    cityBtn.setAttribute('class', 'city')
+    searchContainer.appendChild(cityBtn);
+    localStorage.setItem('CityName', JSON.stringify(cityName));
+
+    getcity(city);
+
+  } else {
+    alert("please enter city");
+  }
 };
 
 citySearchBtn.addEventListener('click', search);
-
 
